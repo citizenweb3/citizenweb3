c61c222aa2e3b8e0179e601679471f5238d99dc2
m1amgn <medvedev.mgn@gmail.com>
2025-10-14 17:49:34 +0700

    changed header arrows

--- src/components/header-arrows/header-arrow.tsx
+++ src/components/header-arrows/header-arrow.tsx
@@ -8,10 +8,10 @@ interface OwnProps {
 
 const HeaderArrow: FC<OwnProps> = ({ isActive, alt }) => (
   <div
-    className={`w-8 h-5 transition-all duration-500 ${
+    className={`w-8 h-5 transition-all duration-600 ${
       isActive
-        ? 'opacity-100 brightness-150 drop-shadow-[0_0_8px_rgba(255,255,255,0.8)] scale-110'
-        : 'opacity-60 brightness-75'
+        ? 'opacity-100 brightness-150 scale-110'
+        : 'opacity-95 brightness-95'
     }`}
   >
     <Image src="/arrow-down.svg" alt={alt} width={36} height={24} className="w-full h-full object-cover" />
